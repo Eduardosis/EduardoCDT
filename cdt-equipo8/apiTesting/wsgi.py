d8be58ad8ab140e82ba1690f50403c61fec7f708
Eduardosis <edytl115@gmail.com>
2024-09-04 14:41:31 -0700

    simon15

--- cdt-equipo8/apiTesting/wsgi.py
+++ cdt-equipo8/apiTesting/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apiTesting.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cdt-equipo8.settings')
 application = get_wsgi_application()
+
